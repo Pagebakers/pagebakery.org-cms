eb2e13a3f27ff249ff0bada18391e8d1c9cf03da
Geartjan <info@geartjan.nl>
2008-12-02 14:04:13 +0100

    routes changed

--- plugins/pagebakery/config/routes.php
+++ plugins/pagebakery/config/routes.php
@@ -10,6 +10,6 @@ Router::connect('/' . Configure::read('Routes.pagebakery') . '/login', array('co
 Router::connect('/' . Configure::read('Routes.pagebakery') . '/logout', array('controller' => 'users', 'action' => 'logout', 'prefix' => 'pb', 'plugin' => 'pagebakery'));
 
 // Admin routes
-Router::connect('/' . Configure::read('Routes.pagebakery') . '/:controller/*', array('action' => 'index', 'prefix' => 'pb', 'pb' => true, 'plugin' => 'pagebakery'));
 Router::connect('/' . Configure::read('Routes.pagebakery') . '/:controller/:action/*', array('prefix' => 'pb', 'pb' => true, 'plugin' => 'pagebakery'));
+Router::connect('/' . Configure::read('Routes.pagebakery') . '/:controller/*', array('action' => 'index', 'prefix' => 'pb', 'pb' => true, 'plugin' => 'pagebakery'));
 ?>
