a0e5e861fb521f4015cbd4613814530eaddc2544
Georgi Momchilov <georgi@ovalpixels.com>
2008-12-24 15:15:15 +0200

    installer integrated

--- webroot/index.php
+++ webroot/index.php
@@ -1,5 +1,5 @@
 <?php
-/* SVN FILE: $Id: index.php 7296 2008-06-27 09:09:03Z gwoo $ */
+/* SVN FILE: $Id: index.php 6311 2008-01-02 06:33:52Z phpnut $ */
 /**
  * Short description for file.
  *
@@ -21,13 +21,13 @@
  * @package			cake
  * @subpackage		cake.app.webroot
  * @since			CakePHP(tm) v 0.2.9
- * @version			$Revision: 7296 $
- * @modifiedby		$LastChangedBy: gwoo $
- * @lastmodified	$Date: 2008-06-27 05:09:03 -0400 (Fri, 27 Jun 2008) $
+ * @version			$Revision: 6311 $
+ * @modifiedby		$LastChangedBy: phpnut $
+ * @lastmodified	$Date: 2008-01-02 00:33:52 -0600 (Wed, 02 Jan 2008) $
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 /**
- * Use the DS to separate the directories in other defines
+ * Do not change
  */
 	if (!defined('DS')) {
 		define('DS', DIRECTORY_SEPARATOR);
@@ -35,36 +35,29 @@
 /**
  * These defines should only be edited if you have cake installed in
  * a directory layout other than the way it is distributed.
- * When using custom settings be sure to use the DS and do not add a trailing DS.
- */
-
-/**
- * The full path to the directory which holds "app", WITHOUT a trailing DS.
- *
+ * Each define has a commented line of code that explains what you would change.
  */
 	if (!defined('ROOT')) {
+		//define('ROOT', 'FULL PATH TO DIRECTORY WHERE APP DIRECTORY IS LOCATED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
+		//You should also use the DS define to separate your directories
 		define('ROOT', dirname(dirname(dirname(__FILE__))));
 	}
-/**
- * The actual directory name for the "app".
- *
- */
 	if (!defined('APP_DIR')) {
+		//define('APP_DIR', 'DIRECTORY NAME OF APPLICATION');
 		define('APP_DIR', basename(dirname(dirname(__FILE__))));
 	}
 /**
- * The absolute path to the "cake" directory, WITHOUT a trailing DS.
- *
+ * This only needs to be changed if the cake installed libs are located
+ * outside of the distributed directory structure.
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', '/Users/contact/Projects/Cake_1.2');
+		//define ('CAKE_CORE_INCLUDE_PATH', 'FULL PATH TO DIRECTORY WHERE CAKE CORE IS INSTALLED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
+		//You should also use the DS define to separate your directories
+		define('CAKE_CORE_INCLUDE_PATH', ROOT);
 	}
-
-/**
- * Editing below this line should not be necessary.
- * Change at your own risk.
- *
- */
+///////////////////////////////
+//DO NOT EDIT BELOW THIS LINE//
+///////////////////////////////
 	if (!defined('WEBROOT_DIR')) {
 		define('WEBROOT_DIR', basename(dirname(__FILE__)));
 	}
@@ -72,7 +65,8 @@
 		define('WWW_ROOT', dirname(__FILE__) . DS);
 	}
 	if (!defined('CORE_PATH')) {
-		if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
+		if (function_exists('ini_set')) {
+			ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'));
 			define('APP_PATH', null);
 			define('CORE_PATH', null);
 		} else {
@@ -81,16 +75,20 @@
 		}
 	}
 	if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
-		trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
+		trigger_error("Can't find CakePHP core.  Check the value of CAKE_CORE_INCLUDE_PATH in app/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
 	}
 	if (isset($_GET['url']) && $_GET['url'] === 'favicon.ico') {
 		return;
 	} else {
+        //PB is not installed - run installer
+		if( !file_exists( CONFIGS . 'database.php' ) ){
+            if( !isset( $_GET['url'] ) || strpos( $_GET['url'], 'installer' ) === false )
+                $url = array( 'controller' => 'installer' );
+		}
 		$Dispatcher = new Dispatcher();
 		$Dispatcher->dispatch($url);
 	}
-/*
-	if (Configure::read() > 0) {
+/*	if (Configure::read() > 0) {
 		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
 	}
 */
