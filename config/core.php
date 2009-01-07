5a39614eef8447359921843ba5057bb9a060951f
Ferdinant Visser <turnred@xxlBase.dev.turnred.net>
2009-01-07 16:54:28 +0100

    test 2

--- config/core.php
+++ config/core.php
@@ -41,7 +41,7 @@
  * In development mode, you need to click the flash message to continue.
  */
 
-	Configure::write('debug', 1);
+	Configure::write('debug', 2);
 
 /**
  * Application wide charset encoding
