f81b62fa97dc087d2c04c9e4946b2a7c74f966fe
Ferdinant <ferdinant@orpheus.turnred.net>
2009-01-12 18:53:45 +0100

    simplified user validation in the model using built-in cakephp validation rules

--- models/user.php
+++ models/user.php
@@ -3,33 +3,26 @@ class User extends AppModel {
 
     public $validate = array(
         'username' => array(
-			'allowedChars' => array(
-				'rule' => 'alphaNumeric',
-				'message' => 'only letters and digits are allowed', 
-				'required' => true
-				),
-			'unique'	=> array(
-				'rule' => array('userExist'),
-				'message' => 'a user with this username already exists',
-				)
+			'validUsername' => array(
+				'rule' => 'alphaNumeric'
+			),
+			'uniqueUsername' => array(
+				'rule' => 'isUnique'
+			)
         ),
 		'email' => array(
-			'allowedChars' => array(
-				'rule' => 'email',
-				'message' => 'this is not a valid e-mail address',
-				'required' => true
-				),
-			'unique'	=> array(
-				'rule' => array('emailExist'),
-				'message' => 'a user with this email address already exists',
-				)
+			'validEmail' => array(
+				'rule' => 'email'
+			),
+			'uniqueEmail' => array(
+				'rule' => 'isUnique'
+			)
         ),
         'passwd' => array(
             'rule' => array('minLength', 4)
         ),
         'passwd2' => array(
-            'rule' => array('comparePassword'),
-            'required' => false
+            'rule' => array('comparePassword')
         )
     );
 
@@ -49,37 +42,7 @@ class User extends AppModel {
         }
         return true;
     }
-
-	/**
-	 * check if username already exists
-	 * @return bool, true if username exist, false otherwise
-	 */
-	public function userExist(){
-		$params = array(
-			'conditions' => array('User.username' => $this->data['User']['username']),
-			'recursive' => -1
-			);
-		if(isset($this->data['User']['username'])){
-			$results = $this->find('count',$params);
-		}
-		return ($results > 0) ? false : true;
-	}
 	
-	/**
-	 * check if email already exists
-	 * @return bool, true if email exist, false otherwise
-	 */
-	public function emailExist(){
-		$params = array(
-			'conditions' => array('User.email' => $this->data['User']['email']),
-			'recursive' => -1
-			);
-		if(isset($this->data['User']['email'])){
-			$results = $this->find('count',$params);
-		}
-		return ($results > 0) ? false : true;
-	}	
-
     public function comparePassword(&$data) {
         return $data['passwd2'] == $this->data['User']['passwd'];
     }
