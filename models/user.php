44ab75b68bd2f8566f1119cdecd40852bd1c37f9
Ferdinant <ferdinant@orpheus.turnred.net>
2009-01-12 14:37:26 +0100

    Added some user validation to prevent duplicate usernames and passwords

--- models/user.php
+++ models/user.php
@@ -3,7 +3,26 @@ class User extends AppModel {
 
     public $validate = array(
         'username' => array(
-            'rule' => 'alphaNumeric'
+			'allowedChars' => array(
+				'rule' => 'alphaNumeric',
+				'message' => 'only letters and digits are allowed', 
+				'required' => true
+				),
+			'unique'	=> array(
+				'rule' => array('userExist'),
+				'message' => 'a user with this username already exists',
+				)
+        ),
+		'email' => array(
+			'allowedChars' => array(
+				'rule' => 'email',
+				'message' => 'this is not a valid e-mail address',
+				'required' => true
+				),
+			'unique'	=> array(
+				'rule' => array('emailExist'),
+				'message' => 'a user with this email address already exists',
+				)
         ),
         'passwd' => array(
             'rule' => array('minLength', 4)
@@ -15,12 +34,11 @@ class User extends AppModel {
     );
 
     public function beforeValidate() {
-        // if left empty, remove from data array
+        // if password field left empty, remove from data array
         if((isset($this->data['User']['passwd']) && empty($this->data['User']['passwd'])) && (isset($this->data['User']['passwd2']) && empty($this->data['User']['passwd2']))) {
             unset($this->data['User']['passwd']);
             unset($this->data['User']['passwd2']);
         }
-
         return true;
     }
 
@@ -32,6 +50,35 @@ class User extends AppModel {
         return true;
     }
 
+	/**
+	 * check if username already exists
+	 * @return bool, true if username exist, false otherwise
+	 */
+	public function userExist(){
+		$params = array(
+			'conditions' => array('User.username' => $this->data['User']['username']),
+			'recursive' => -1
+			);
+		if(isset($this->data['User']['username'])){
+			$results = $this->find('count',$params);
+		}
+		return ($results > 0) ? false : true;
+	}
+	
+	/**
+	 * check if email already exists
+	 * @return bool, true if email exist, false otherwise
+	 */
+	public function emailExist(){
+		$params = array(
+			'conditions' => array('User.email' => $this->data['User']['email']),
+			'recursive' => -1
+			);
+		if(isset($this->data['User']['email'])){
+			$results = $this->find('count',$params);
+		}
+		return ($results > 0) ? false : true;
+	}	
 
     public function comparePassword(&$data) {
         return $data['passwd2'] == $this->data['User']['passwd'];
