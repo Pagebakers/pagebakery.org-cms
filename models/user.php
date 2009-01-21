3034df8948840683c4f16f3fc8f526c9c5955d4c
Ferdinant Visser <turnred@xxlBase.dev.turnred.net>
2009-01-21 13:49:49 +0100

    updated readme file

--- models/user.php
+++ models/user.php
@@ -22,22 +22,29 @@ class User extends AppModel {
             'rule' => array('minLength', 4)
         ),
         'passwd2' => array(
-            'rule' => array('comparePassword')
+			'minLength' => array(            
+				'rule' => array('minLength', 4)
+			),
+			'comparePass' => array(            
+				'rule' => array('comparePassword')
+			)			
         )
     );
 
     public function beforeValidate() {
-        // if password field left empty, remove from data array
-        if((isset($this->data['User']['passwd']) && empty($this->data['User']['passwd'])) && (isset($this->data['User']['passwd2']) && empty($this->data['User']['passwd2']))) {
-            unset($this->data['User']['passwd']);
-            unset($this->data['User']['passwd2']);
-        }
-        return true;
+        // if password field left empty on update (edit), remove from data array
+		if (isset($this->data['User']['id'])){
+			if((isset($this->data['User']['passwd']) && empty($this->data['User']['passwd'])) && (isset($this->data['User']['passwd2']) && empty($this->data['User']['passwd2']))) {
+				unset($this->data['User']['passwd']);
+				unset($this->data['User']['passwd2']);
+			}
+		}
+        return true; 
     }
 
     public function beforeSave() {
         // Hash the password
-        if(isset($this->data['User']['passwd'])) {
+        if (isset($this->data['User']['passwd'])) {
             $this->data['User']['password'] = Security::hash(Configure::read('Security.salt') . $this->data['User']['passwd']);
         }
         return true;
