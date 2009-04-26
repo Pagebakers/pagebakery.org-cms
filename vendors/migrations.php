3bdf70b51881334248aaef1f1b6b49dab5ffb81b
Georgi Momchilov <georgi@ovalpixels.com>
2009-04-26 12:56:27 +0300

    migrations vendor update

--- vendors/migrations.php
+++ vendors/migrations.php
@@ -1,14 +1,14 @@
 <?php
 /**
 * Migrations -- A Simple PHP Class to convert YAML into SQL queries
-* @version 0.1 -- 2008-04-08
+* @version 0.2 -- 2008-04-08
 * @author Georgi Momchilov <gmomchilov@gmail.com>
 * @copyright Copyright 2008 Georgi Momchilov
 * @license http://www.opensource.org/licenses/mit-license.php MIT License
 * @package Migrations
 */
 
-App::import('Vendor','spyc');
+App::import('vendor','spyc');
 App::import('Core', 'ConnectionManager');
 
 class Migrations{
@@ -135,6 +135,9 @@ class Migrations{
                     elseif( $sAction == 'rename_table' || $sAction == 'rename_tables'){
                         $this->aTasks[$sDirection][] = $this->rename_table( $sName, $aProperties['name'] );
                     }
+                    elseif( $sAction == 'merge_table' || $sAction == 'merge_tables' ){
+                        $this->aTasks[$sDirection] = am( $this->aTasks[$sDirection], $this->merge_table( $sName, $aProperties ) );
+                    }
                     elseif( $sAction == 'truncate_table' || $sAction == 'truncate_tables'){
                         $this->aTasks[$sDirection][] = $this->truncate_table( $sName );
                     }
@@ -224,7 +227,117 @@ class Migrations{
         $sSql .= ');';
         return $sSql;
     }
+    
+    /**
+    * Generate a SQL array for merging a table. If no_id and/or no_dates is specified, it will autogenerate an id field and dates fields ( created, modified )
+    *
+    * @param string $sTable Table name
+    * @param array $aFields Array of fields in format array('field'=>array('type'=>'int','length'=>10,'null'=>true,'primary'=>true,'auto_increment'=>true))
+    */
+    function merge_table($sTable, $aFields = array()){
+        //check whether that table exists at all
+        if( !in_array( $this->getPrefix().$sTable, $this->oDb->listSources() ) ){
+            return array( $this->create_table( $sTable, $aFields ) );
+        }
+        
+        //table does not exist - merge it
+        $aQueries = array();
+        $aIndexes = array();
+        $aCurrentFields = $this->oDb->describe( new Model( false, $sTable ) );
+        
+        //no_id and no_dates first
 
+        //there should be an id according to the new schema
+        if( !in_array( 'no_id', $aFields ) ){
+            $aIdFormat = ( $this->bUse_uuid ) ? $this->aUuid_format : $this->aId_format;
+            //but there is no such one in the current schema
+            if( !array_key_exists( 'id', $aCurrentFields ) ){
+                $aQueries[] = $this->add_field( $sTable, array( 'id' => $aIdFormat ) );
+                $aQueries[] = 'ALTER TABLE '.$this->getPrefix().$sTable.' ADD '.$this->oDb->buildIndex( array('PRIMARY' => array( 'column' => array('id') ) ) );
+            }
+        }
+        //there should be no id - drop it
+        else if( array_key_exists( 'id', $aCurrentFields ) ){
+            $aQueries[] = $this->drop_field( $sTable, 'id' );
+            unset( $aFields[ array_search( 'no_id', $aFields, true ) ] );
+        }
+
+        //work with the current fields - drop or alter
+        foreach( $aCurrentFields as $sCurrentFieldName => $aCurrentFieldProperties )
+                if( $sCurrentFieldName != 'id' &&
+                    $sCurrentFieldName != 'created' &&
+                    $sCurrentFieldName != 'modified'
+                  ){
+            //field doesn't exist anymore
+            if( !array_key_exists( $sCurrentFieldName, $aFields ) ){
+                $aQueries[] = $this->drop_field( $sTable, $sCurrentFieldName );
+            }
+            else{
+                //cope with null
+                if( in_array( 'not_null', $aFields[ $sCurrentFieldName ] ) ){
+                    $aFields[ $sCurrentFieldName ]['null'] = false;
+                    unset( $aFields[ $sCurrentFieldName ][ array_search( 'not_null', $aFields[ $sCurrentFieldName ], true ) ] );
+                }
+                elseif( in_array( 'is_null', $aFields[ $sCurrentFieldName ] ) ){
+                    $aFields[ $sCurrentFieldName ]['null'] = true;
+                    unset( $aFields[ $sCurrentFieldName ][ array_search( 'is_null', $aFields[ $sCurrentFieldName ], true ) ] );
+                }
+                else{
+                    $aFields[ $sCurrentFieldName ]['null'] = true;
+                }
+                
+                //properties are different
+                if( $aCurrentFieldProperties != $aFields[ $sCurrentFieldName ] ){
+                    $aQueries[] = $this->alter_field( $sTable, array( $sCurrentFieldName => $aFields[ $sCurrentFieldName ] ) );
+                }
+                
+                //keys ( except for primary! ) - add keys only if they don't already exist
+                $aKeys = array();
+                if( !empty( $aFields[ $sCurrentFieldName ]['unique'] ) && 
+						( !isset( $aCurrentFieldProperties['key'] ) || $aCurrentFieldProperties['key'] != "unique" )
+						){
+                    $aQueries[] = $this->add_key( $sTable, array( 'unique' => $sCurrentFieldName ) );
+                }
+                if( !empty( $aFields[ $sCurrentFieldName ]['index'] ) && 
+						( !isset( $aCurrentFieldProperties['key'] ) || $aCurrentFieldProperties['key'] != "index" )
+						){
+                    $aQueries[] = $this->add_key( $sTable, array( 'index' => $sCurrentFieldName ) );
+                }
+                        
+                unset( $aFields[ $sCurrentFieldName ] );
+            }
+        }
+        //then - add any new fields if necessary
+        foreach( $aFields as $sFieldName => $aFieldProperties )if( is_string( $sFieldName ) ){
+            $aQueries[] = $this->add_field( $sTable, array( $sFieldName => $aFieldProperties ) );
+            //keys ( except for primary! )
+            $aKeys = array();
+            if( !empty( $aFields[ $sFieldName ]['unique'] ) ){
+                $aQueries[] = $this->add_key( $sTable, array( 'unique' => $sFieldName ) );
+            }
+            if( !empty( $aFields[ $sFieldName ]['index'] ) ){
+                $aQueries[] = $this->add_key( $sTable, array( 'index' => $sFieldName ) );
+            }
+        }
+        
+        //there should be dates fields according to the new schema
+        if( !in_array( 'no_dates', $aFields ) ){
+            //but there is no such one in the current schema
+            if( !array_key_exists( 'created', $aCurrentFields )  ){
+                $aQueries[] = $this->add_field( $sTable, array( 'created' => array( 'type' => 'datetime' ) ) );
+                $aQueries[] = $this->add_field( $sTable, array( 'modified' => array( 'type' => 'datetime' ) ) );
+            }
+            unset( $aFields[ array_search( 'no_dates', $aFields, true ) ] );
+        }
+        //there should be no dates fields - drop 'em
+        else if( array_key_exists( 'created', $aCurrentFields ) ){
+            $aQueries[] = $this->drop_field( $sTable, 'created' );
+            $aQueries[] = $this->drop_field( $sTable, 'modified' );
+        }
+                
+        return $aQueries;
+    }
+        
     /**
     * Generate SQL for rename table
     */
@@ -276,7 +389,17 @@ class Migrations{
         $sSql = trim( $sSql, ", \n\t" );
         return $sSql;
     }
-
+    
+    /**
+    * Generate SQL for keys ( index and unique )
+    */
+    function add_key( $sTable, $aKey ){
+        $sType = key( $aKey );
+        $sColumn = $aKey[ $sType ];
+        $sSql = 'ALTER TABLE '.$this->getPrefix().$sTable.' ADD '.$this->oDb->buildIndex( array( $sColumn => array( $sType => true, 'column' => $sColumn ) ) );
+        return $sSql;
+    }
+        
     /**
     * If there is a query - just return it
     */
