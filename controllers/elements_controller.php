7b24fbe22de497836e19ac4fa3e52056d4e52926
Ferdinant Visser <ferdinant@brouwerij.turnred.lan>
2009-05-02 17:46:12 +0200

    added view and template for lostpassword email. added lostpassword method to users controller

--- controllers/elements_controller.php
+++ controllers/elements_controller.php
@@ -6,5 +6,14 @@
 
 class ElementsController extends AppController {
 
+    /**
+     * update content of current element type
+     */
+    public function update(){
+        if ($this->$class->update()){
+            // dikke wojo! tis gelukt
+        }
+    }
+
 }
 ?>
---
+++ views/elements/email/text/lostpassword.ctp
@@ -0,0 +1,5 @@
+Hello <?php print $user['User']['name']; ?>,
+
+You are receiving this e-mail because you (or someone else) asked for it using
+the `lost password` option at the pagebakery login screen.
+
