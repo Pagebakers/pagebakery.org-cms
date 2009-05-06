358667fb7e800daf7339283f80e9031b294d8a14
Ferdinant Visser <ferdinant@brouwerij.turnred.lan>
2009-05-06 17:52:07 +0200

    updated some stuff to the elements controller

--- controllers/elements_controller.php
+++ controllers/elements_controller.php
@@ -10,9 +10,26 @@ class ElementsController extends AppController {
      * update content of current element type
      */
     public function update(){
-        if ($this->$class->update()){
-            // dikke wojo! tis gelukt
+        $result = array('success' => false);
+
+        // get element id from POST
+        $element_id = (int) $this->params['form']['element_id'];
+
+        /**
+         * Load element to see what class/model is used
+         */
+        $this->Element->recursive = 0;
+        $element = $this->Element->read(array('id','class'), $element_id);
+
+        // init model of element type
+        $Element =& ClassRegistry::init($element['Element']['class']);
+        if (is_object($Element)) {
+            if ($Element->save($this->data)){
+                $result = array('success' => true);
+            }
         }
+
+        $this->set(compact('result'));
     }
 
 }
